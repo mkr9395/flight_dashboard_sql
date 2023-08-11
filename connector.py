457a1809d67d8abd00f124395e10c25d50042e49
Mohit Kumar <mkr9395@gmail.com>
2023-08-11 15:37:55 +0530

    meassage needs to be passedsecond changes

---
+++ connector.py
@@ -0,0 +1,25 @@
+# pip install mysql.connector
+import mysql
+
+from mysql import connector as c
+from mysql.connector import Error
+
+
+
+try:
+    conn = c.connect(host="localhost",user="root",password="mohit@123")
+    if conn.is_connected():
+        cursor = conn.cursor()
+        print("succesfully connected")
+        
+        # create a database
+        cursor.execute("CREATE DATABASE flights;")
+        conn.commit()
+except Error as e:
+    print("Error while connecting to MySQL", e)
+
+#finally:
+   # if conn.is_connected():
+       # cursor.close()
+       # conn.close()
+    
